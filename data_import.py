c026e27a70863c726a73cab1cfdfcd21e7bfa5b5
ssqskn <ssqskn@163.com>
2015-03-20 22:38:46 +0800

    new file:   data/train_item_sample.csv 	new file:   data/train_user_sample.csv 	new file:   data_import.py 	modified:   data_process.py

---
+++ data_import.py
@@ -0,0 +1,98 @@
+#coding=utf-8
+
+import csv
+
+def readData(itemSize, userSize):   ##size indicates how many records to be involved
+    PATH = "data\\"
+    FILE_ITEM = "tianchi_mobile_recommend_train_item.csv"
+    FILE_USER = "tianchi_mobile_recommend_train_user.csv"
+    ##read file for item
+    f = open(PATH + FILE_ITEM)
+    item_obj    = csv.reader(f)
+    train_item  = []
+    header_item = {}
+    for i,line in enumerate(item_obj):
+        if i == 0:
+            for ind,item in enumerate(line):
+                header_item[item] = ind
+        elif (i > 0 and i <= itemSize):
+            train_item.append(line)
+        else:
+            break
+    f.close
+    ##read file for user
+    f = open(PATH + FILE_USER)
+    user_obj    = csv.reader(f)
+    train_user  = []
+    header_user = {}
+    for i,line in enumerate(user_obj):
+        if i == 0:
+            for ind,user in enumerate(line):
+                header_user[user] = ind
+        elif (i > 0 and i <= userSize):
+            train_user.append(line)
+        else:
+            break
+    f.close
+
+    return train_item, header_item, train_user, header_user
+
+
+def readSampleData():
+    PATH = "data\\"
+    FILE_ITEM = "train_item_sample.csv"
+    FILE_USER = "train_user_sample.csv"
+    ##read file for item
+    f = open(PATH + FILE_ITEM)
+    item_obj    = csv.reader(f)
+    train_item  = []
+    header_item = {}
+    for i,line in enumerate(item_obj):
+        if i == 0:
+            for ind,item in enumerate(line):
+                header_item[item] = ind
+        else:
+            train_item.append(line)
+    f.close
+    ##read file for user
+    f = open(PATH + FILE_USER)
+    user_obj    = csv.reader(f)
+    train_user  = []
+    header_user = {}
+    for i,line in enumerate(user_obj):
+        if i == 0:
+            for ind,user in enumerate(line):
+                header_user[user] = ind
+        else:
+            train_user.append(line)
+    f.close
+    
+    return train_item, header_item, train_user, header_user
+    
+
+
+if __name__ == '__main__':
+    SAVE_SAMPLE_DATA = True
+    
+    train_item, header_item, train_user, header_user = readData(itemSize = 20000, userSize = 100000)
+
+    if SAVE_SAMPLE_DATA:
+        f = open("data\\train_item_sample.csv",'wb')
+        writer = csv.writer(f)
+        writer.writerow(['item_id','item_geohash','item_category'])
+        for line in train_item:
+            writer.writerow(line)
+        f.close
+        
+        f = open("data\\train_user_sample.csv",'wb')
+        writer = csv.writer(f)
+        writer.writerow(['user_id','item_id','behavior_type','user_goehash','item_category','time'])
+        for line in train_user:
+            writer.writerow(line)
+        f.close
+    
+    print header_item
+    print train_item[0:10]
+    print header_user
+    print train_user[0]
+    
--- data_process.py
+++ data_process.py
@@ -1,3 +1,24 @@
 #coding=utf-8
 
-import numpy as np
+from data_import import readData,readSampleData
+
+def train_item_process(data, header):
+
+    pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    train_item, header_item, train_user, header_user = readSampleData()
+#   train_item, header_item, train_user, header_user = readData(itemSize = 1000000, userSize = 10000000)
+    train_item_process(train_item, header_item)
+    
+    print header_item
+    print train_item[0]
