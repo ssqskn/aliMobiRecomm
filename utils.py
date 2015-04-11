d461ae43f39cba2db4b58e35fc98ee60e666844a
ssqskn <ssqskn@163.com>
2015-04-11 14:09:46 +0800

    deleted:    pickle/categoryFeatures.pickle 	deleted:    pickle/categoryFeatures_ALL.csv 	deleted:    pickle/categoryFeatures_ALL.pickle 	deleted:    pickle/itemFeatures.pickle 	deleted:    pickle/userCategoryFeatures.pickle 	deleted:    pickle/userFeatures.pickle 	deleted:    pickle/userFeatures_ALL.csv 	deleted:    pickle/userFeatures_ALL.pickle 	deleted:    pickle/userItemFeatures.pickle 	modified:   utils.py

--- utils.py
+++ utils.py
@@ -28,11 +28,17 @@ def data_sort(train_user):
     train_user = train_user.sort(columns = ['user_category_pairs','D&H','item_id'])
     return train_user    
 
-def str2num_scale(train):
+def str2num_scale(train,TEST = False):
     header = train.columns
     for item in header:
         train[item] = train[item].apply(func = lambda x:round(float(x),3) if type(x) == str else x)
-        if not item == 'if_pay':
+        if not TEST:
+            #if not item == 'if_pay':
+            if not item == 22:
+                itemAvg     = train[item].mean(axis = 0)
+                itemStd     = train[item].std(axis = 0)
+                train[item] = train[item].apply(func = lambda x: (x - itemAvg) * 1.0/itemStd)
+        else:
             itemAvg     = train[item].mean(axis = 0)
             itemStd     = train[item].std(axis = 0)
             train[item] = train[item].apply(func = lambda x: (x - itemAvg) * 1.0/itemStd)
@@ -46,6 +52,7 @@ def shuffling(train):
     train = train.sort_index(by = ['rand'])
     header = train.columns
     train  = pd.DataFrame(np.array(train), columns = header)
+    del train['rand']
     return train
 
 def sqlConnect():
